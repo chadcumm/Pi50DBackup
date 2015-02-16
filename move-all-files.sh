47c296cab9509a080c329ff13a4b2cb2cc7d67fb
chadcumm <algeron@gmail.com>
2015-02-16 03:55:58 +0000

    rmeoved loop

--- move-all-files.sh
+++ move-all-files.sh
@@ -6,21 +6,6 @@ lockdir=/tmp/Pi50DBackupMove.lock
      # Remove lockdir when the script finishes, or when it receives a signal
      trap 'rm -rf "$lockdir"' 0    # remove directory when script finishes
  else
-	counter=1
-	while [ $counter -le 7 ]
-	do
-	echo $counter
-	echo >&2 "waiting 60 seconds to try again"
-     	sleep 60s
-     	if mkdir "$lockdir"
-      	 then
-          echo >&2 "successfully acquired lock"
-          # Remove lockdir when the script finishes, or when it receives a signal
-          trap 'rm -rf "$lockdir"' 0    # remove directory when script finishes
-	  break
-         fi
-	 ((counter++))
-	 done
      echo >&2 "cannot acquire lock, giving up on $lockdir"
      exit 0
 fi
